01ce16af49e765d3027174572641695befbd841d
Dimanche Jhon Steeve <jsteeve@noukod.com>
2018-11-06 21:16:02 +0000

    automatically backed up by learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -1,6 +1,6 @@
 module Players
   class Computer < Player
-    move = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+    @@move = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
     def move(board)
       move.sample(1)
     end

8961bcf3e214e515172176e7de01bcecc0148404
Dimanche Jhon Steeve <jsteeve@noukod.com>
2018-11-06 20:31:12 +0000

    automatically backed up by learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -2,7 +2,7 @@ module Players
   class Computer < Player
     move = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
     def move(board)
-      self.getAnInteger
+      move.rand
     end
   end
 end
